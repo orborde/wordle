d39e4c57f0a1688f80eb4f0eb132a20e3ef5e568
William Ehlhardt <williamehlhardt@gmail.com>
2022-01-11 01:11:58 -0800

    hint func

--- wordle.py
+++ wordle.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python3
 
 import argparse
+import collections
+from enum import Enum
 import pathlib
 
 parser = argparse.ArgumentParser(description='Wordle solver')
@@ -13,4 +15,54 @@ parser.add_argument(
 
 args = parser.parse_args()
 WORDS = set(args.dictionary.read_text().splitlines())
-print(WORDS)
+print(len(WORDS), 'words loaded from', args.dictionary)
+
+class Hint(Enum):
+    GREEN = 'GREEN'
+    YELLOW = 'YELLOW'
+    GRAY = 'GRAY'
+
+    def __repr__(self):
+        return self.value
+
+def hint(actual, guess):
+    """Returns the hint for the word guessed.
+
+    >>> hint('abcd', 'abcd')
+    [GREEN, GREEN, GREEN, GREEN]
+    >>> hint('abcd', 'dcba')
+    [YELLOW, YELLOW, YELLOW, YELLOW]
+    >>> hint('abcde', 'edcba')
+    [YELLOW, YELLOW, GREEN, YELLOW, YELLOW]
+    >>> hint('xxxxx', 'bacon')
+    [GRAY, GRAY, GRAY, GRAY, GRAY]
+    >>> hint('xaaax', 'xxaaa')
+    [GREEN, YELLOW, GREEN, GREEN, YELLOW]
+    >>> hint('aabbc', 'bbxxa')
+    [YELLOW, YELLOW, GRAY, GRAY, YELLOW]
+    >>> hint('bbxxa', 'aabbc')
+    [YELLOW, GRAY, YELLOW, YELLOW, GRAY]
+    """
+    if len(actual) != len(guess):
+        raise ValueError('Word lengths must match')
+
+    floating_letter_counts = collections.Counter(actual)
+    for ac, gc in zip(actual, guess):
+        if ac == gc:
+            floating_letter_counts[ac] -= 1
+
+    out = []
+    for ac, gc in zip(actual, guess):
+        if ac == gc:
+            out.append(Hint.GREEN)
+        elif floating_letter_counts[gc] > 0:
+            out.append(Hint.YELLOW)
+            floating_letter_counts[gc] -= 1
+        else:
+            out.append(Hint.GRAY)
+
+    return out
+        
+
+import doctest
+doctest.testmod()
