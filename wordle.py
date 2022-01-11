d04dded2bcc448fc2f38da0396329f1a76a3f20b
William Ehlhardt <williamehlhardt@gmail.com>
2022-01-11 02:24:00 -0800

    hack up recursive search

--- wordle.py
+++ wordle.py
@@ -3,7 +3,9 @@
 import argparse
 import collections
 from enum import Enum
+from fractions import Fraction
 import pathlib
+from typing import List, Tuple, Dict, Set, Optional
 
 parser = argparse.ArgumentParser(description='Wordle solver')
 parser.add_argument(
@@ -13,9 +15,12 @@ parser.add_argument(
     default='wordle_dictionary.txt',
     )
 
-args = parser.parse_args()
-WORDS = set(args.dictionary.read_text().splitlines())
-print(len(WORDS), 'words loaded from', args.dictionary)
+parser.add_argument(
+    '--hints',
+    help='Hints received so far. Example: grant:GGYRR',
+    type=str,
+    default='',
+)
 
 class Hint(Enum):
     GREEN = 'GREEN'
@@ -24,6 +29,13 @@ class Hint(Enum):
 
     def __repr__(self):
         return self.value
+
+# I don't know how to manage namespaces cleanly.
+GREEN = Hint.GREEN
+YELLOW = Hint.YELLOW
+GRAY = Hint.GRAY
+
+ALL_GREEN = [GREEN, GREEN, GREEN, GREEN, GREEN]
 
 def hint(actual, guess):
     """Returns the hint for the word guessed.
@@ -58,15 +70,49 @@ def hint(actual, guess):
     out = []
     for ac, gc in zip(actual, guess):
         if ac == gc:
-            out.append(Hint.GREEN)
+            out.append(GREEN)
         elif floating_letter_counts[gc] > 0:
-            out.append(Hint.YELLOW)
+            out.append(YELLOW)
             floating_letter_counts[gc] -= 1
         else:
-            out.append(Hint.GRAY)
+            out.append(GRAY)
 
     return out
-        
 
-import doctest
-doctest.testmod()
+GuessWithExpectation = collections.namedtuple('GuessWithExpectation', ['guess', 'expected_after'])
+def best_guess(possibilities: Set[str]) -> GuessWithExpectation:
+    return min(
+        [
+            GuessWithExpectation(guess, expected_guesses_after(possibilities, guess))
+            for guess in possibilities
+        ],
+        key=lambda g: g.expectation,
+    )
+
+def expected_guesses_after(possibilities: Set[str], guess) -> Fraction:
+    possibilities_by_hint = collections.defaultdict(list)
+    for actual in possibilities:
+        possibilities_by_hint[hint(actual, guess)].append(actual)
+
+    remaining_guesses_distribution = collections.Counter()
+    for hint_, sub_possibilities in possibilities_by_hint.items():
+        assert len(sub_possibilities) > 0
+
+        if hint_ == ALL_GREEN:
+            assert len(sub_possibilities) == 1
+            remaining_guesses_distribution[0] += 1
+        else:
+            g = best_guess(sub_possibilities)
+            remaining_guesses_distribution[g.expected_after + 1] += len(sub_possibilities)
+
+def parse_hints(hintstr: str):
+    pass
+
+if __name__ == '__main__':
+    import doctest
+    failures, _ = doctest.testmod()
+    assert failures == 0
+
+    args = parser.parse_args()
+    WORDS = set(args.dictionary.read_text().splitlines())
+    print(len(WORDS), 'words loaded from', args.dictionary)
